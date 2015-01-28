9dfced0675de3d4054b92b549e376bd0b519a0ea
Bryan Bumgardner <bryanbumgardner@gmail.com>
2015-01-27 19:56:53 -0600

    1-27
    Fixed the whole organization.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
+
+
+var playerData;
+
 $(document).ready(function() {
     console.log("Hello world.")
+    getData();
 });
 
 
+function getData() {
+	$.getJSON("js/ChaseHeadley.json", function(data){
+		console.log(data);
+		playerData = data;
+
+		drawStuff();
+
+	});
 
 
+}
 
+function drawStuff() {
+
+
+	$(".chart").html(playerData.name);
+
+	$.each(playerData.stats, function(i, item){
+		$(".chart").append("<p>"+item.AVG+"</p>");
+
+	});
+
+
+}
 
